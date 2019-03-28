c763b26b7fc40e1b71fc4cf3d041a615edf02bf9
wxy <3285934296@qq.com>
2019-03-28 20:26:37 +0800

    再次测试github

--- src/com/literary/web/controller/AboutMeSentenceController.java
+++ src/com/literary/web/controller/AboutMeSentenceController.java
@@ -1,9 +1,10 @@
 package com.literary.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("aboutMeSentenceController")
 public class AboutMeSentenceController {
 
 }
